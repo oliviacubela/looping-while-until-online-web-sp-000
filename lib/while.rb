e935fa2550fa335d1bcbfd43a239cffc1dbdb890
Olivia Cubela <oliviacubela@gmail.com>
2020-05-12 13:58:29 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
-def using_while(phrase)
-  levitation_force = 6
-
-while levitation_force < 10
-  levitation_force += 1
-  puts "Wingardium Leviosa"
+def using_while
+  counter = 6
+  while counter < 10
+    puts "Wingardium Leviosa"
+    counter += 1
+  end
 end
